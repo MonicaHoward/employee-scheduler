655c6886371ce13db86ae5926fb6270e92b3128a
Monica Howard <mo@Monicas-Air.attlocal.net>
2019-12-29 21:18:38 -0600

    adds query selectors for all columns and form elements

---
+++ script.js
@@ -0,0 +1,20 @@
+// table columns
+var shiftDate = document.querySelector(".shift-date");
+var employeeFirst = document.querySelector(".employee-first");
+var employeeLast = document.querySelector(".employee-last");
+var dept = document.querySelector(".dept");
+var shiftStart = document.querySelector(".shift-start");
+var shiftEnd = document.querySelector(".shift-end");
+var shiftLength = document.querySelector(".shift-length");
+
+// form input
+var shiftDateInput = document.querySelector("#shift-date-input");
+var deptInput = document.querySelector("#dept-input");
+var employeeFirstInput = document.querySelector("#employee-first-input");
+var employeeLastInput = document.querySelector("#employee-last-input");
+var shiftStartInput = document.querySelector("#shift-start-input");
+var shiftEndInput = document.querySelector("#shift-end-input");
+var shiftLengthInput = document.querySelector("#shift-length-input");
+
+// submit button
+var addShiftButton = document.querySelector("#add-shift-button");
